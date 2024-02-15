e2630db9b4f33b442870c345bb748b1fcbc44916
Théo lavabo <alexispernette@outlook.fr>
2024-02-15 18:42:19 +0100

    headerButtonOnOff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
 const buttonBurger = document.querySelector('.containBurgerMenuOpen');
 const burgerWrapper = document.querySelector('.burWrapper');
+const buttonOn = document.querySelector('.buttonOn');
+const rondOn = document.querySelector('.rondOn');
+const on = document.querySelector('.on')
 
 buttonBurger.addEventListener('click', function () {
     burgerWrapper.classList.toggle('burgerWrapper');
     burgerWrapper.classList.toggle('display')
+})
+
+buttonOn.addEventListener('click', function () {
+    buttonOn.classList.toggle('buttonOn');
+    buttonOn.classList.toggle('buttonOff');
+    rondOn.classList.toggle('rondOn');
+    rondOn.classList.toggle('rondOff');
+    on.classList.toggle('on');
+    on.classList.toggle('off');
 })
