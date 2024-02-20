e897c564b934c0b7546b295e34c7c6e8f7346033
Théo lavabo <alexispernette@outlook.fr>
2024-02-20 01:10:18 +0100

    news en js 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,23 +30,40 @@ const containNews = document.querySelector('.containNews');
 
 for (let i = 0; i < 3; i++) {
     let div = document.createElement('div');
-    div.classList.add('wrapperContainNews')
+    div.classList.add('wrapperContainNews');
     containNews.append(div);
-    let wrapperContainNews = document.querySelectorAll('.wrapperContainNews')
+    let wrapperContainNews = document.querySelectorAll('.wrapperContainNews');
     let htre = document.createElement('h3');
-    htre.textContent = (newsListings[i].title)
+    htre.textContent = (newsListings[i].title);
     htre.classList.add('titleNews');
-    wrapperContainNews[i].append(htre)
+    wrapperContainNews[i].append(htre);
     let p = document.createElement('p');
-    p.classList.add('pNews')
-    p.textContent = (newsListings[i].content)
-    wrapperContainNews[i].append(p)
-    let divContainA = document.createElement('div')
-    divContainA.classList.add('containANews')
-    wrapperContainNews[i].append(divContainA)
+    p.classList.add('pNews');
+    p.textContent = (newsListings[i].content);
+    wrapperContainNews[i].append(p);
+    let divContainA = document.createElement('div');
+    divContainA.classList.add('containANews');
+    wrapperContainNews[i].append(divContainA);
     let containANews = document.querySelectorAll('.containANews');
-    let a = document.createElement('a')
-    a.textContent = "En savoir plus"
-    a.classList.add('aNews')
-    containANews[i].append(a)
+    let a = document.createElement('a');
+    a.textContent = "En savoir plus";
+    a.classList.add('aNews');
+    containANews[i].append(a);
 }
+
+//end news js voir all
+
+let a = document.createElement('a');
+a.classList.add('containMostNews');
+a.href = ('#');
+containNews.append(a);
+let containANews = document.querySelector('.containMostNews');
+let p = document.createElement('p');
+p.classList.add('mostNewsP');
+p.textContent = 'Voir les autres news';
+containANews.append(p);
+let iHtml = document.createElement('i')
+iHtml.classList.add('mostNewsI')
+iHtml.classList.add('fa-arrow-right')
+iHtml.classList.add('fa-solid')
+containANews.append(iHtml)
